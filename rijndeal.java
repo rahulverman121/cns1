8a63cf34f0d74d8e8cb3cc1d72e96415ac72c9fb
rahulverman121 <98263712+rahulverman121@users.noreply.github.com>
2022-07-01 14:55:38 +0530

    Update rijndeal.java

--- rijndeal.java
+++ rijndeal.java
@@ -1,4 +1,33 @@
 
+
+	import java.io.*;
+	import java.util.Scanner;
+	import javax.crypto.*;
+	import java.security.*;
+	import java.math.BigInteger;
+	import javax.crypto.Cipher;
+	import javax.crypto.KeyGenerator;
+	import javax.crypto.SecretKey;
 public class rijndeal {
 
-}
+	
+		public static void main(String args[]) throws Exception
+		{
+			Scanner sc=new Scanner(System.in);
+			System.out.println("enter a message");
+			String message=sc.nextLine();
+			KeyGenerator kg = KeyGenerator.getInstance("AES");
+			kg.init(128);
+			SecretKey sk=kg.generateKey();
+			byte[] raw=sk.getEncoded();
+			SecretKeySpace sks=new SecretKeySpace(raw,"AES");
+			Cipher c=Cipher.getInstance("AES");
+			c.init(Cipher.ENCRYPT_MODE,sks);
+			byte[]encrypt=c.doFinal(message.getBytes());
+			System.out.println("encrypted string"+new String(encrypt));
+			System.out.println("encrypted string hex"+asHex(encrypt));
+			
+				
+		}
+	}
+
